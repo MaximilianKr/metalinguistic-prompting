7978fe2bbaf01e8533fdaa1dba7dca3f4a223ea2
przvlprd <przvlprd@pm.me>
2024-04-25 20:48:21 +0200

    remove commented code block

--- scripts/run_exp1_hf.sh
+++ scripts/run_exp1_hf.sh
@@ -27,22 +27,3 @@ fi
 python run_exp1_word-prediction.py $MODEL $REVISION $QUANTIZATION $DATAFILE $OUTFILE
 
 echo "All tasks completed successfully. Files stored in'"${RESULTDIR}"'"
-
-# for EVAL_TYPE in "direct" "metaQuestionSimple" "metaInstruct" "metaQuestionComplex"; do
-#     OUTFILE="${RESULTDIR}/${CORPUS}_${SAFEMODEL}_${EVAL_TYPE}.json"
-    
-#     # By default, we won't save the full vocab distributions.
-#     # Uncomment the two lines below if you'd like to.
-    
-#     # DISTFOLDER="${RESULTDIR}/dists/${CORPUS}_${SAFEMODEL}_${EVAL_TYPE}"
-#     # mkdir -p $DISTFOLDER
-
-#     echo "Running Experiment 1 (word prediction): model = ${MODEL}; eval_type = ${EVAL_TYPE}"
-#     python run_exp1_word-prediction.py \
-#         --model $MODEL \
-#         --model_type "hf" \
-#         --eval_type ${EVAL_TYPE} \
-#         --data_file $DATAFILE --out_file ${OUTFILE}
-#         # Uncomment the line below to save full distributions.
-#         # --dist_folder $DISTFOLDER
-# done
