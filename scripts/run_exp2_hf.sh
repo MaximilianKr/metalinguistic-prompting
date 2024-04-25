94e695ec66ac6e5f0c3076046091271121ba2803
przvlprd <przvlprd@pm.me>
2024-04-25 20:53:22 +0200

    remove commented code

--- scripts/run_exp2_hf.sh
+++ scripts/run_exp2_hf.sh
@@ -15,13 +15,13 @@ SAFEMODEL=$(echo $MODEL | cut -d '/' -f 2)
 RESULTDIR="results/exp2_word-comparison"
 DATAFILE="datasets/exp2/${CORPUS}/corpus.csv"
 
+mkdir -p $RESULTDIR
+
 if [ "$QUANTIZATION" != "full" ]; then
     OUTFILE="${RESULTDIR}/${CORPUS}_${SAFEMODEL}_${QUANTIZATION}_${REVISION}"
 else
     OUTFILE="${RESULTDIR}/${CORPUS}_${SAFEMODEL}_${REVISION}"
 fi
-
-mkdir -p $RESULTDIR
 
 python run_exp2_word-comparison.py $MODEL $REVISION $QUANTIZATION $DATAFILE $OUTFILE
 
--- scripts/run_exp3a_hf.sh
+++ scripts/run_exp3a_hf.sh
@@ -27,38 +27,3 @@ fi
 python run_exp3a_sentence-judgment.py $MODEL $REVISION $QUANTIZATION $DATAFILE $OUTFILE
 
 echo "All tasks completed successfully. Files stored in'"${RESULTDIR}"'"
-
-
-# # Helper function
-# run_experiment () {
-#     # Capture relevant variables
-#     local EVAL_TYPE=$1
-
-#     # Define variable-dependent file/folder names
-#     OUTFILE="${RESULTDIR}/${CORPUS}_${SAFEMODEL}_${EVAL_TYPE}.json"
-    
-#     # By default, we won't save the full vocab distributions.
-#     # Uncomment the two lines below if you'd like to.
-    
-#     # DISTFOLDER="${RESULTDIR}/dists/${CORPUS}_${SAFEMODEL}_${EVAL_TYPE}"
-#     # mkdir -p $DISTFOLDER
-
-#     # Run the evaluation script
-#     echo "Running Experiment 3a (sentence judgment): model = ${MODEL}; eval_type = ${EVAL_TYPE}" >&2
-#     python run_exp3a_sentence-judgment.py \
-#         --model $MODEL \
-#         --model_type "hf" \
-#         --eval_type ${EVAL_TYPE} \
-#         --data_file $DATAFILE --out_file ${OUTFILE}
-#         # Uncomment the line below to save full distributions.
-#         # --dist_folder $DISTFOLDER
-# }
-
-# NOTE: "direct" model is THE SAME across 3a and 3b
-# FIRST, run the "direct" model.
-run_experiment "direct"
-
-# # NEXT, run the other models.
-for EVAL_TYPE in "metaQuestionSimple" "metaInstruct" "metaQuestionComplex"; do
-    run_experiment "${EVAL_TYPE}"
-done
